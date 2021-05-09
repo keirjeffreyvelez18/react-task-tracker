0b99558082332577a625ba9d15aea76b96876835
keirjeffreyvelez18 <32511317+keirjeffreyvelez18@users.noreply.github.com>
2021-05-10 01:56:01 +0800

    Task Tracker

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+
+
+const Tasks = ({tasks}) => {
+    return (
+        <>
+            {tasks.map((tasks) => (
+                <h3 key={tasks.id}>{tasks.text}</h3>))}
+        </>  
+    )
+}
+
+export default Tasks
