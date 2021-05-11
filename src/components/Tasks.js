884100f9949b01950c0506af48e59606e84c7ea5
keirjeffreyvelez18 <32511317+keirjeffreyvelez18@users.noreply.github.com>
2021-05-12 01:27:19 +0800

    Start build/Json server

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,11 @@
+import Task from "./Task"
 
-
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <>
-            {tasks.map((tasks) => (
-                <h3 key={tasks.id}>{tasks.text}</h3>))}
+            {tasks.map((task, index) => (
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+            ))}
         </>  
     )
 }
